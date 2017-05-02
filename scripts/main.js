7944d5436aba550b6bc858cbad880a6978eb2588
aperkaz <alainperkaz@gmail.com>
2017-05-02 14:56:13 +0200

    Added emojis

--- scripts/main.js
+++ scripts/main.js
@@ -2,18 +2,16 @@ var groups = [];
 var vegGroups = [];
 var pack =  [];
 
+/* General methods*/
 function addGroup(){
   var newGroup = extractGroupInfo();
   if(newGroup.name !== ''){
     if(newGroup.vegetarian === true){
       vegGroups.push(newGroup);
-      console.log(vegGroups);
     }else {
       groups.push(newGroup);
-      console.log(groups);
     }
     addGroupToList(newGroup);
-    console.log('--');
    }
 }
 
@@ -31,75 +29,15 @@ function extractGroupInfo(){
   return newGroup;
 }
 
-/*  Group Matching Algorithing*/
-function generateResults(){
-  cleanTable();
-  shuffleGroups();
-}
-
-
-function shuffleGroups(){
-
-  pack = [];
-
-  var groupsClone = jQuery.extend(true, [], groups);
-  var vegGroupsClone = vegGroups.slice(0);
-  console.log(groups);
-  console.log(groupsClone);
-  console.log(vegGroups);
-  console.log('vegs: ',vegGroupsClone);
-
-  // match veggies
-  while(vegGroupsClone.length > 0 && vegGroupsClone.length !== "undefined"){
-    console.log('matching vegies');
-    var index = Math.floor(Math.random() * vegGroupsClone.length);
-    console.log('index: ',index)
-    console.log('Adding veg: ',vegGroupsClone[index]);
-    pack.push(vegGroupsClone[index]);
-    vegGroupsClone.splice(index,1);
-    if(pack.length >= 3){
-      console.log('Publishing: ',pack);
-       publishGroup(pack);
-       pack = [];
-    }
- }
-
- // fill veggie groups
-  while(pack.length > 0 && pack.length < 3 && groupsClone.length > 0){
-    console.log('filling vegie group');
-
-      console.log('Adding normal: '+groupsClone[0]);
-      pack.push(groupsClone[0]);
-      groupsClone.splice(0,1);
-
-  }
-  console.log('Publishing: ',pack);
-  publishGroup(pack);
-  pack = [];
-
-  // match normals
-  while( groupsClone.length > 0){
-
-    var index = Math.floor(Math.random() * groupsClone.length );
-    console.log('Adding normal: ',groupsClone[index]);
-    pack.push(groupsClone[index]);
-    groupsClone.splice(index,1);
-    if(pack.length >= 3){
-       console.log('Publishing: ',pack);
-       publishGroup(pack);
-       pack = [];
-    }
-  }
-  if(pack.length > 0){
-    console.log('OUT: ',pack);
-    publishGroup(pack);
-  }
-  pack = [];
-}
-
 /* Group List Methods */
 function addGroupToList(newGroup){
-  appendHTML('added-groups', '<div>'+newGroup.name+'</div>');
+  if(newGroup.vegetarian === true){
+    appendHTML('added-groups', '<div>'+newGroup.name+ ' &#127813;</div>');
+  } else {
+    appendHTML('added-groups', '<div>'+newGroup.name+' &#127831;</div>');
+  }
+  document.getElementById('group-name').value = "";
+  document.getElementById('group-vegetarian').checked = false;
 }
 
 function cleanList(){
@@ -128,25 +66,4 @@ function publishGroup(pack){
 
 function cleanTable(){
   setHTML('group-list', '');
-}
-
-/*  HTML Manipulation Methods*/
-function setHTML(id, html){
-  document.getElementById(id).innerHTML = html;
-}
-
-function appendHTML(id, html){
-  document.getElementById(id).innerHTML += html;
-}
-
-/* Array Manipulation Methods */
-function deepCopy(oldObj) {
-    var newObj = oldObj;
-    if (oldObj && typeof oldObj === 'object') {
-        newObj = Object.prototype.toString.call(oldObj) === "[object Array]" ? [] : {};
-        for (var i in oldObj) {
-            newObj[i] = deepCopy(oldObj[i]);
-        }
-    }
-    return newObj;
 }
